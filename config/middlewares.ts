98999a3fe8aa40955e93175858b584270c8c4f68
Shalini648404 <shalinibaswaraju@gmail.com>
2024-11-29 15:02:42 +0530

    Added readme file

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,33 @@
-export default [
+/*export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];*/
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: false,
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://jazzy-pavlova-f4e46b.netlify.app'], // Add your frontend's URL here
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
